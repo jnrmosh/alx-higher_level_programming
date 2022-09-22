8f9fd2d30a73f9e345def14466ef91804d8b7829
jnrmosh <omondimichaelotieno@gmail.com>
2022-09-22 12:24:57 -0700

    print number and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,11 +7,11 @@ def main():
         print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
-        print(f"1: {(sys.argv[1])}")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print(f"{argc:d} arguments:")
+        print("{} arguments:".format(argc))
         for i in range(1, argc + 1):
-            print("{i}: {(sys.argv[i])}")
+            print("{}: {}".format(i, sys.argv[i]))
 
 if __name__ == "__main__":
     main()
